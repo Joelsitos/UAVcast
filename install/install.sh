67be24e69aaf31a262ce615930fcfc399764ba3d
Bernt Christian Egeland <cbegelan@gmail.com>
2018-07-30 20:28:03 +0000

    v0.4.5

--- install/install.sh
+++ install/install.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
+logfile=build.log
 
 . config.sh
+. spinner.sh
 
+#Include date time for logging
+dt=$(date '+%d/%m/%Y %H:%M:%S');
+echo "$(tput setaf 2)Starting UAVcast Installation.$(tput sgr0)  $dt" | tee  $logfile 2>&1 
+
+start_spinner
+echo -ne '#                         (1%)\r'
 # Get current Directory
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
@@ -9,6 +17,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 Basefolder="$(cd ../; pwd)" 
 
 # Create systemctl for easy stop/start/restart
+echo -ne '#####                     (25%)\r'
 Systemd=$DIR/"systemd"
 main="$MAINPID"
 if [ ! -d "$Systemd" ] 
@@ -39,50 +48,61 @@ EOM
 chmod 644 $FILE
 cp $FILE /lib/systemd/system/
 
-sudo systemctl daemon-reload
+echo -ne '#######                   (30%)\r'
+sudo systemctl daemon-reload >> $logfile 2>&1 
 # sudo systemctl enable $FILE
 
 # If RPI 3, we need to remap the UART pins
-set_dtoverlay_pi_three
+set_dtoverlay_pi_three >> $logfile 2>&1 
 
+echo -ne '#########                 (35%)\r'
 # set config for cmdline.txt and config.txt
-do_serial
+do_serial >> $logfile 2>&1 
 
 # This will ensure that all configured network devices are up and have an IP address assigned before boot continues.
-sudo systemctl enable systemd-networkd-wait-online.service
+sudo systemctl enable systemd-networkd-wait-online.service >> $logfile 2>&1 
 
 # Update and Upgrade the Pi, otherwise the build may fail due to inconsistencies
-sudo apt-get update -y 
+echo -ne '##########                (40%)\r'
+sudo apt-get update -y >> $logfile 2>&1 
 
 # Get the required libraries
 sudo apt-get install -y --force-yes jq dnsutils inadyn usb-modeswitch modemmanager network-manager openvpn network-manager-openvpn \
-                                     dh-autoreconf gstreamer1.0-tools gstreamer1.0-plugins-good gstreamer1.0-plugins-bad
+                                     dh-autoreconf gstreamer1.0-tools gstreamer1.0-plugins-good gstreamer1.0-plugins-bad >> $logfile 2>&1 
 #                                 wvdial build-essential cmake libboost-all-dev libconfig++-dev libreadline-dev
 
+echo -ne '#############             (50%)\r'
 #Args Options for installing web interface
 args=$1                           
 argsToLower=$(echo "$args" | tr '[:upper:]' '[:lower:]')
 case $argsToLower in
-          "web")
-            echo Installing UAVcast including Web interface
+        "web")
             #Run Web instalation
-            sudo sh $DIR/./web.sh
+            sudo $DIR/./web.sh >> $logfile 2>&1
         ;;
         *)
-         printf "\n\n NOTE!!!  Installing UAVcast without Web Interface \n\n\n use web argurment ( ./install.sh web ) to install web UI.\n\n\n\n"
-        ;;
-esac
+         printf "\n\n NOTE!!!  Installing UAVcast without Web Interface \n\n use web argurment ( ./install.sh web ) to install web UI.\n\n" | tee  $logfile 2>&1 
 
+esac
+echo -ne '###############           (60%)\r'
 ################# COMPILE UAV software ############
 #UAVcast dependencies
-mkdir $Basefolder/packages
+mkdir -p $Basefolder/packages
 cd $Basefolder/packages
 
+echo -ne '#################         (70%)\r'
 #Mavproxy
-git clone https://github.com/UAVmatrix/cmavnode.git
+git clone https://github.com/UAVmatrix/cmavnode.git >> $logfile 2>&1 
 
+echo -ne '####################      (80%)\r'
 # Create symlink to cmavnode
-sudo ln -s $Basefolder/usr/bin/cmavnode /usr/bin/cmavnode
+sudo ln -s $Basefolder/usr/bin/cmavnode /usr/bin/cmavnode >> $logfile 2>&1
 
+echo -ne '##########################(100%)\r'
+sleep 1
+echo -ne '\n'
+stop_spinner $?
+sleep 1
 # Completed
-printf "\n\n\nInstallastion completed. \n Reboot RPI and access UAVcast webinterface \n by opening your browser and type the IP of RPI.\n"
+printf "$(tput setaf 2)\n\n\ntype 'cat build.log' to view the installation output"
+printf "$(tput setaf 2)\n\n\nUAVcast Installastion completed. \n Reboot RPI and access UAVcast webinterface \n by opening your browser and type the IP of RPI.\n$(tput sgr0)" | tee  $logfile 2>&1 
--- install/web.sh
+++ install/web.sh
@@ -10,37 +10,35 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 Basefolder="$(cd ../; pwd)" 
 
 # WEB SERVICES INSTALLATION
-echo "Installing Node & NPM"
+echo 'Installing nodejs...'
    if is_pione; then
-      echo "Pi 1"
-      wget -O - https://raw.githubusercontent.com/sdesalas/node-pi-zero/master/install-node-v6.11.3.sh | bash
-      # Add support for node CLI tools
-      export PATH=$PATH:/opt/nodejs/bin
-      # install pm2 web server
-      sudo npm install pm2@latest -g
-      sudo ln -s /opt/nodejs/bin/pm2 /usr/bin/pm2
+      echo "Pi-1 Detected"
    elif is_pione_w; then
-      echo "Pi0w"
-      wget -O - https://raw.githubusercontent.com/sdesalas/node-pi-zero/master/install-node-v6.11.3.sh | bash
-      # Add support for node CLI tools
-      export PATH=$PATH:/opt/nodejs/bin
-      # install pm2 web server
-      sudo npm install pm2@latest -g
-      sudo ln -s /opt/nodejs/bin/pm2 /usr/bin/pm2
+      echo "Pi0w Detected"
    elif is_pitwo; then
-      echo "Pi 2"
-      curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
-      sudo apt-get install -y nodejs 
-      # install pm2 web server
-      sudo npm install pm2@latest -g
+      echo "Pi-2 Detected"
    else
-      echo "Pi 3"
-      curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
-      sudo apt-get install -y nodejs 
-      # install pm2 web server
-      sudo npm install pm2@latest -g
+      echo "Pi-3 Detected"
    fi
 
+sudo node -v | grep -q v10 > /dev/null
+if [ ! $? == 0 ]; then
+    echo "Installing Node version 10"
+    sudo wget -O - https://raw.githubusercontent.com/audstanley/NodeJs-Raspberry-Pi/master/Install-Node.sh | sudo bash;
+    else
+    echo "Node 10 exsist, skipping"
+fi
+
+# install pm2 web server
+if [ ! -x /usr/bin/pm2 ]; then
+    echo "PM2 not found, lets install it.."
+    sudo npm install pm2@2.9 -g
+    sudo chmod +x /opt/nodejs/bin/pm2
+    sudo ln -sfn /opt/nodejs/bin/pm2 /usr/bin/pm2
+    sudo rm -rf ~/.pm2
+else
+echo "PM2 Exsist, skipping.."
+fi
 
 cd $Basefolder/web
 sudo npm install --production
